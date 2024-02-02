1462735bf7baa19dfee914f20f3ab46496aa7a6f
Saul Paredes <saulkurosaki13@gmail.com>
2024-02-02 15:05:56 -0600

    feat(Navbar): implement conditional rendering for the user photo and share work button or the auth providers component

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,11 @@
 import { NavLinks } from "@/constants";
 import Image from "next/image";
 import Link from "next/link";
+import AuthProviders from "./AuthProviders";
 
 const Navbar = () => {
+  const session = {};
+
   return (
     <nav className="flexBetween navbar">
       <div className="flex-1 flexStart gap-10">
@@ -17,6 +20,17 @@ const Navbar = () => {
             </Link>
           ))}
         </ul>
+      </div>
+
+      <div className="flexCenter gap-4">
+        {session ? (
+          <>
+            UserPhoto
+            <Link href="/create-project">Share Work</Link>
+          </>
+        ) : (
+          <AuthProviders />
+        )}
       </div>
     </nav>
   );
