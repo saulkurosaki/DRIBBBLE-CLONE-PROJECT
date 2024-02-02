d5a1162d4f7d2c62f7b231fd0432ac42fa0129cd
Saul Paredes <saulkurosaki13@gmail.com>
2024-02-02 14:39:01 -0600

    feat(Navbar): implement Navbar component initial layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,6 @@
+import Navbar from "@/components/Navbar";
 import "./globals.css";
+import Footer from "@/components/Footer";
 
 export const metadata = {
   title: "Flexibble",
@@ -13,9 +15,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        Navbar
+        <Navbar />
         <main>{children}</main>
-        Footer
+        <Footer />
       </body>
     </html>
   );
