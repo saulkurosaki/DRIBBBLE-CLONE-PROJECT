3d77acbb8108ceb60fb661c91f8819fdb76a71df
Saul Paredes <saulkurosaki13@gmail.com>
2024-02-02 15:03:59 -0600

    feat(AuthProviders): Implement AuthPoviders component initial layout

---
+++ components/AuthProviders.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AuthProviders = () => {
+  return (
+    <div>
+      <h3>Auth Providers Component Test c:</h3>
+    </div>
+  );
+};
+
+export default AuthProviders;
