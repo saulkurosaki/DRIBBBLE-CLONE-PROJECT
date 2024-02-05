aea4347bd9f619ac224b3ca1f05d223dafd09028
Saul Paredes <saulkurosaki13@gmail.com>
2024-02-05 13:07:38 -0600

    grafbase redeploy 14 commit

--- grafbase.config.ts
+++ grafbase.config.ts
@@ -3,38 +3,33 @@ import { graph, config, auth } from "@grafbase/sdk";
 const g = graph.Standalone();
 
 // @ts-ignore
-const User = g
-  .type("User", {
-    name: g.string().length({ min: 2, max: 100 }),
-    email: g.string().unique(),
-    avatarUrl: g.url(),
-    description: g.string().length({ min: 2, max: 1000 }).optional(),
-    githubUrl: g.url().optional(),
-    linkedinUrl: g.url().optional(),
-    projects: g
-      .relation(() => Project)
-      .list()
-      .optional(),
-  })
-  .auth((rules: any) => {
-    rules.public().read();
-  });
+const User = g.type("User", {
+  name: g.string(),
+  email: g.string(),
+  avatarUrl: g.url(),
+  description: g.string().optional(),
+  githubUrl: g.url().optional(),
+  linkedinUrl: g.url().optional(),
+  projects: g.ref(Project).list().optional(),
+});
+// .auth((rules: any) => {
+//   rules.public().read();
+// });
 
 // @ts-ignore
-const Project = g
-  .type("Project", {
-    title: g.string().length({ min: 3 }),
-    description: g.string(),
-    image: g.url(),
-    liveSiteUrl: g.url(),
-    githubUrl: g.url(),
-    category: g.string().search(),
-    createdBy: g.relation(() => User),
-  })
-  .auth((rules: any) => {
-    rules.public().read();
-    rules.private().create().delete().update();
-  });
+const Project = g.type("Project", {
+  title: g.string(),
+  description: g.string(),
+  image: g.url(),
+  liveSiteUrl: g.url(),
+  githubUrl: g.url(),
+  category: g.string(),
+  createdBy: g.ref(User),
+});
+// .auth((rules: any) => {
+//   rules.public().read();
+//   rules.private().create().delete().update();
+// });
 
 // const jwt = auth.JWT({
 //   issuer: "grafbase",
