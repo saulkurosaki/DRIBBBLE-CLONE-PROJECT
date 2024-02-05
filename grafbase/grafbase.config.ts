63171170e8806c225c0e5a67981a3c6c1b5b18a0
Saul Paredes <saulkurosaki13@gmail.com>
2024-02-05 12:17:15 -0600

    grafbase redeploy 5 commit

--- grafbase/grafbase.config.ts
+++ grafbase/grafbase.config.ts
@@ -1,42 +1,47 @@
-import { graph, config } from "@grafbase/sdk";
-
-// Welcome to Grafbase!
-//
-// Configure authentication, data sources, resolvers and caching for your GraphQL API.
+import { graph, config, auth } from "@grafbase/sdk";
 
 const g = graph.Standalone();
 
-// Data Sources - https://grafbase.com/docs/connectors
-//
-// const pg = connector.Postgres('pg', { url: g.env('DATABASE_URL') })
-// g.datasource(pg)
+// @ts-ignore
+const User = g
+  .model("User", {
+    name: g.string().length({ min: 2, max: 100 }),
+    email: g.string().unique(),
+    avatarUrl: g.url(),
+    description: g.string().length({ min: 2, max: 1000 }).optional(),
+    githubUrl: g.url().optional(),
+    linkedinUrl: g.url().optional(),
+    projects: g.ref(Project).list().optional(),
+  })
+  .auth((rules: any) => {
+    rules.public().read();
+  });
 
-// Resolvers - https://grafbase.com/docs/resolvers
-//
-// g.query('helloWorld', {
-//   returns: g.string(),
-//   resolver: 'hello-world',
+// @ts-ignore
+const Project = g
+  .model("Project", {
+    title: g.string().length({ min: 3 }),
+    description: g.string(),
+    image: g.url(),
+    liveSiteUrl: g.url(),
+    githubUrl: g.url(),
+    category: g.string().search(),
+    createdBy: g.ref(User),
+  })
+  .auth((rules: any) => {
+    rules.public().read();
+    rules.private().create().delete().update();
+  });
+
+// const jwt = auth.JWT({
+//   issuer: 'grafbase',
+//   secret:  g.env('NEXTAUTH_SECRET')
 // })
 
 export default config({
   graph: g,
-  // Authentication - https://grafbase.com/docs/auth
-  auth: {
-    // OpenID Connect
-    // const oidc = auth.OpenIDConnect({ issuer: g.env('OIDC_ISSUER_URL') })
-    // providers: [oidc],
-    rules: (rules) => {
-      rules.public();
-    },
-  },
-  // Caching - https://grafbase.com/docs/graphql-edge-caching
-  // cache: {
-  //   rules: [
-  //     {
-  //       types: ['Query'], // Cache everything for 60 seconds
-  //       maxAge: 60,
-  //       staleWhileRevalidate: 60
-  //     }
-  //   ]
-  // }
+  // auth: {
+  //   providers: [jwt],
+  //   rules: (rules) => rules.private()
+  // },
 });
