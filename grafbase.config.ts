2eea56167473a0e2d3a5631673d81a44d7852268
Saul Paredes <saulkurosaki13@gmail.com>
2024-02-05 12:46:01 -0600

    grafbase redeploy 11 commit

--- grafbase.config.ts
+++ grafbase.config.ts
@@ -2,7 +2,7 @@ import { g, config, auth } from "@grafbase/sdk";
 
 // @ts-ignore
 const User = g
-  .model("User", {
+  .type("User", {
     name: g.string().length({ min: 2, max: 100 }),
     email: g.string().unique(),
     avatarUrl: g.url(),
@@ -20,7 +20,7 @@ const User = g
 
 // @ts-ignore
 const Project = g
-  .model("Project", {
+  .type("Project", {
     title: g.string().length({ min: 3 }),
     description: g.string(),
     image: g.url(),
