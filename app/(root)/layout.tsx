7a6778d37fec95e25d3c93875635b2557d764829
Saul Paredes <saulkurosaki13@gmail.com>
2024-02-08 12:27:29 -0600

    fix(root/layout.tsx): fix little layout component semantic bug

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -8,12 +8,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body>
-        <Navbar />
-        <main>{children}</main>
-        <Footer />
-      </body>
-    </html>
+    <div>
+      <Navbar />
+      <main>{children}</main>
+      <Footer />
+    </div>
   );
 }
