454ef03eaf9aec053aa57a542639139666c37920
Saul Paredes <saulkurosaki13@gmail.com>
2024-02-08 16:56:39 -0600

    feat(CreateProject): implement the user authentication

--- app/(root)/create-project/page.tsx
+++ app/(root)/create-project/page.tsx
@@ -1,13 +1,25 @@
 import Modal from "@/components/Modal";
 import ProjectForm from "@/components/ProjectForm";
+import { getUserById } from "@/lib/actions/user.actions";
+import { auth, clerkClient } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 import React from "react";
 
-const CreateProject = () => {
+const CreateProject = async () => {
+  const { userId } = auth();
+  const clerkUser = await clerkClient.users.getUser(userId as string);
+  const user_id = String(clerkUser.publicMetadata.userId);
+  const user = await getUserById(user_id);
+
+  if (!user) {
+    redirect("/");
+  }
+
   return (
     <Modal>
       <h3 className="modal-head-text">Create a New Project</h3>
 
-      <ProjectForm />
+      <ProjectForm type="create" user={user} />
     </Modal>
   );
 };
