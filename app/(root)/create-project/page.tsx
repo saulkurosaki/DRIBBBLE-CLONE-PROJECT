338fcb68854390c7a5492b5ad94b3e6a289b5a6b
Saul Paredes <saulkurosaki13@gmail.com>
2024-02-08 16:38:19 -0600

    feat(Modal): implement the Modal component overlay and wrapper modal basic structure

--- app/(root)/create-project/page.tsx
+++ app/(root)/create-project/page.tsx
@@ -1,7 +1,15 @@
+import Modal from "@/components/Modal";
+import ProjectForm from "@/components/ProjectForm";
 import React from "react";
 
 const CreateProject = () => {
-  return <div>Create Project Page Test c:</div>;
+  return (
+    <Modal>
+      <h3 className="modal-head-text">Create a New Project</h3>
+
+      <ProjectForm />
+    </Modal>
+  );
 };
 
 export default CreateProject;
--- components/Modal.tsx
+++ components/Modal.tsx
@@ -1,7 +1,30 @@
-import React from "react";
+"use client";
 
-const Modal = () => {
-  return <div>Modal Component</div>;
+import { useCallback, useRef, ReactNode } from "react";
+import { useRouter } from "next/navigation";
+import Image from "next/image";
+import { Button } from "./ui/button";
+
+const Modal = ({ children }: { children: ReactNode }) => {
+  const overlay = useRef<HTMLDivElement>(null);
+  const wrapper = useRef<HTMLDivElement>(null);
+  const router = useRouter();
+
+  const handleClick = () => {};
+
+  const onDismiss = () => {};
+
+  return (
+    <div ref={overlay} className="modal" onClick={handleClick}>
+      <Button onClick={onDismiss} className="absolute top-4 right-8">
+        <Image src="/close.svg" alt="close" width={17} height={17} />
+      </Button>
+
+      <div ref={wrapper} className="modal_wrapper">
+        {children}
+      </div>
+    </div>
+  );
 };
 
 export default Modal;
