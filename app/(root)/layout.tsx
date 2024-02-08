da66a9bdb33dc2829c5bb71dc0bf31cf22b15926
Saul Paredes <saulkurosaki13@gmail.com>
2024-02-08 11:47:11 -0600

    refactor the app (root) layout.tsx

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,19 @@
+import Navbar from "@/components/Navbar";
+import "../globals.css";
+import Footer from "@/components/Footer";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>
+        <Navbar />
+        <main>{children}</main>
+        <Footer />
+      </body>
+    </html>
+  );
+}
