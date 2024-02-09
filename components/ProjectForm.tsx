39b5c67a81d80956da059a6750cdecfbe880e5fc
Saul Paredes <saulkurosaki13@gmail.com>
2024-02-09 11:53:22 -0600

    feat(ProjectForm): implement form poster image display

--- components/ProjectForm.tsx
+++ components/ProjectForm.tsx
@@ -1,7 +1,48 @@
-import React from "react";
+"use client";
 
-const ProjectForm = () => {
-  return <div>Project Form Component</div>;
+import { UserProps } from "@/types";
+import Image from "next/image";
+import { ChangeEvent } from "react";
+
+type Props = {
+  type: string;
+  user: UserProps;
+};
+
+const ProjectForm = ({ type, user }: Props) => {
+  const handleFormSubmit = (e: React.FormEvent) => {};
+
+  const handleChangeImage = (e: ChangeEvent<HTMLInputElement>) => {};
+
+  const form = {
+    image: "",
+  };
+
+  return (
+    <form onSubmit={handleFormSubmit} className="flexStart form">
+      <div className="flexStart flex_image-container">
+        <label htmlFor="poster" className="flexCenter flex_image-label">
+          {!form.image && "Choose a poster for your image"}
+        </label>
+        <input
+          id="image"
+          type="file"
+          accept="image/*"
+          required={type === "create"}
+          className="form_image-input"
+          onChange={handleChangeImage}
+        />
+        {form.image && (
+          <Image
+            src={form?.image}
+            alt="Project poster"
+            className="sm:p-10 object-contain z-20"
+            fill
+          />
+        )}
+      </div>
+    </form>
+  );
 };
 
 export default ProjectForm;
