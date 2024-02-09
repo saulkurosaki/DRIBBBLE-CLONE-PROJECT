4deb7b60b43e3759faf4c6c5de757c8da0c76399
Saul Paredes <saulkurosaki13@gmail.com>
2024-02-09 12:18:33 -0600

    feat(ProjectForm): implement description form field

--- components/ProjectForm.tsx
+++ components/ProjectForm.tsx
@@ -18,6 +18,9 @@ const ProjectForm = ({ type, user }: Props) => {
   const form = {
     image: "",
     title: "",
+    description: "",
+    liveSiteUrl: "",
+    githubUrl: "",
   };
 
   return (
@@ -50,6 +53,42 @@ const ProjectForm = ({ type, user }: Props) => {
         placeholder="Flexibble"
         setState={(value) => handleStateChange("title", value)}
       />
+
+      <FormField
+        title="Description"
+        state={form.description}
+        placeholder="Showcase and discover remarkable developer projects"
+        setState={(value) => handleStateChange("description", value)}
+      />
+
+      {/* <FormField
+        type="url"
+        title="Website URL"
+        state={form.liveSiteUrl}
+        placeholder="https://flexibble.com"
+        setState={(value) => handleStateChange("liveSiteUrl", value)}
+      /> */}
+
+      {/* <FormField
+        type="url"
+        title="GitHub URL"
+        state={form.githubUrl}
+        placeholder="https://github.com/saulkurosaki"
+        setState={(value) => handleStateChange("githubUrl", value)}
+      /> */}
+
+      {/* <FormField
+        title="Title"
+        state={form.title}
+        placeholder="Flexibble"
+        setState={(value) => handleStateChange("title", value)}
+      /> */}
+
+      {/* Custom Input Category */}
+
+      <div className="flexStart w-full">
+        <button>Create</button>
+      </div>
     </form>
   );
 };
