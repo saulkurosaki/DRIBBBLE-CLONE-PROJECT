f6a9c3f7a6d81387b54daa868174b0ccad554a8d
Saul Paredes <saulkurosaki13@gmail.com>
2024-02-09 12:08:49 -0600

    feat(FormField): implement complete FormField component

--- components/FormField.tsx
+++ components/FormField.tsx
@@ -1,7 +1,37 @@
-import React from "react";
+import { FormFieldProps } from "@/types";
 
-const FormField = () => {
-  return <div>Form field Component c:</div>;
+const FormField = ({
+  type,
+  title,
+  state,
+  placeholder,
+  isTextArea,
+  setState,
+}: FormFieldProps) => {
+  return (
+    <div className="flexStart flex-col w-full gap-4">
+      <label className="w-full text-gray-100">{title}</label>
+
+      {isTextArea ? (
+        <textarea
+          placeholder={placeholder}
+          value={state}
+          required
+          className="form_field-input"
+          onChange={(e) => setState(e.target.value)}
+        />
+      ) : (
+        <input
+          type={type || "text"}
+          placeholder={placeholder}
+          value={state}
+          required
+          className="form_field-input"
+          onChange={(e) => setState(e.target.value)}
+        />
+      )}
+    </div>
+  );
 };
 
 export default FormField;
