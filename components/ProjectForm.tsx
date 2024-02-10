e479be1263305b1006d717902ea3e5a5f4e1d92f
Saul Paredes <saulkurosaki13@gmail.com>
2024-02-10 16:12:05 -0600

    feat(ProjectForm): implement the handleChangeImage function

--- components/ProjectForm.tsx
+++ components/ProjectForm.tsx
@@ -25,7 +25,27 @@ const ProjectForm = ({ type, user }: Props) => {
 
   const handleFormSubmit = (e: React.FormEvent) => {};
 
-  const handleChangeImage = (e: ChangeEvent<HTMLInputElement>) => {};
+  const handleChangeImage = (e: ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+
+    const file = e.target.files?.[0];
+
+    if (!file) return;
+
+    if (!file.type.includes("image")) {
+      return alert("Please upload an image file");
+    }
+
+    const reader = new FileReader();
+
+    reader.readAsDataURL(file);
+
+    reader.onload() = () => {
+      const result = reader.result as string;
+
+      handleStateChange("image", result);
+    };
+  };
 
   const handleStateChange = (fieldName: string, value: string) => {
     setForm((prevState) => ({ ...prevState, [fieldName]: value }));
