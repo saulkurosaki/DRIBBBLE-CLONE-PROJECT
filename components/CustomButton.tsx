238921a1406ee3f930bd6399757d1b73320f7883
Saul Paredes <saulkurosaki13@gmail.com>
2024-02-10 16:39:06 -0600

    feat(CustomButton): implement complete CustomButton component

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -1,7 +1,38 @@
-import React from "react";
+import { CustomButtonProps } from "@/types";
+import Image from "next/image";
 
-const CustomButton = () => {
-  return <div>Custom Button COmponent Test c:</div>;
+const CustomButton = ({
+  title,
+  leftIcon,
+  rightIcon,
+  handleClick,
+  isSubmitting,
+  type,
+  bgColor,
+  textColor,
+}: CustomButtonProps) => {
+  return (
+    <button
+      type={type || "button"}
+      disabled={isSubmitting}
+      className={`flexCenter gap-3 px-4 py-3
+        ${textColor ? textColor : "text-white"}
+        ${
+          isSubmitting ? "bg-black/50" : bgColor ? bgColor : "bg-primary-purple"
+        }
+        rounded-xl text-sm font-medium max-md:w-full
+      `}
+      onClick={handleClick}
+    >
+      {leftIcon && <Image src={leftIcon} alt="left" width={14} height={14} />}
+
+      {title}
+
+      {rightIcon && (
+        <Image src={rightIcon} alt="right" width={14} height={14} />
+      )}
+    </button>
+  );
 };
 
 export default CustomButton;
