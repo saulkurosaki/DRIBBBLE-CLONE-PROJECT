0c90eb1f802e471c055f12cce6ae9668214fb1b3
Saul Paredes <saulkurosaki13@gmail.com>
2024-02-13 15:17:04 -0600

    feat(home page): implement the projects mapping using the ProjectCard component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,35 @@
-const Home = () => {
+import ProjectCard from "@/components/ProjectCard";
+import { fetchAllProjects } from "@/lib/actions/project.actions";
+import { ProjectInterface } from "@/types";
+
+const Home = async () => {
+  const projects = await fetchAllProjects();
+
+  if (projects.length === 0) {
+    return (
+      <section className="flexStart flex-col paddings">
+        <p className="no-result-text text-center">
+          No projects found, go create some first.
+        </p>
+      </section>
+    );
+  }
+
   return (
     <section className="flex-start flex-col paddings mb-16">
       <h1>Categories</h1>
-      <h1>Posts</h1>
+
+      <section className="projects-grid">
+        {projects.map((project: ProjectInterface) => (
+          <ProjectCard
+            key={project?._id}
+            id={project?._id}
+            image={project?.image}
+            title={project?.title}
+          />
+        ))}
+      </section>
+
       <h1>LoadMore</h1>
     </section>
   );
