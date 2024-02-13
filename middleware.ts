5e1b7b8dc185d84a6e5339190e125dc5c56e4a40
Saul Paredes <saulkurosaki13@gmail.com>
2024-02-13 12:17:30 -0600

    update the clerk middleware.ts file publicRoutes and privateRoutes config arrays

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,8 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-  publicRoutes: ["/", "/api/webhook/clerk"],
-  ignoredRoutes: ["/api/webhook/clerk"],
+  publicRoutes: ["/", "/api/webhook/clerk", "/api/upload"],
+  ignoredRoutes: ["/api/webhook/clerk", "/api/upload"],
 });
 
 export const config = {
