19469984dda2dfb30985874e009ba61089a69818
Saul Paredes <saulkurosaki13@gmail.com>
2024-02-16 21:15:11 -0600

    feat(ProjectActions): implement the handleDeleteProject function

--- components/ProjectActions.tsx
+++ components/ProjectActions.tsx
@@ -1,7 +1,51 @@
-import React from "react";
+"use client";
 
-const ProjectActions = () => {
-  return <div>Project Actions Component test c:</div>;
+import { deleteProject } from "@/lib/actions/project.actions";
+import { ProjectActionsProps } from "@/types";
+import Image from "next/image";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
+
+const ProjectActions = ({ projectId }: ProjectActionsProps) => {
+  const [isDeleting, setIsDeleting] = useState<boolean>(false);
+  const router = useRouter();
+
+  const handleDeleteProject = async () => {
+    setIsDeleting(true);
+
+    try {
+      await deleteProject(projectId);
+
+      router.push("/");
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setIsDeleting(false);
+    }
+  };
+
+  return (
+    <>
+      {/* <Link
+        href={`/edit-project/${projectId}`}
+        className="flexCenter edit-action_btn"
+      >
+        <Image src="/pencile.svg" width={15} height={15} alt="edit" />
+      </Link>
+
+      <button
+        type="button"
+        disabled={isDeleting}
+        className={`flexCenter delete-action_btn ${
+          isDeleting ? "bg-gray" : "bg-primary-purple"
+        }`}
+        onClick={handleDeleteProject}
+      >
+        <Image src="/trash.svg" width={15} height={15} alt="delete" />
+      </button> */}
+    </>
+  );
 };
 
 export default ProjectActions;
