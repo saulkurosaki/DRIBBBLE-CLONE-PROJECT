d41cc652b06f0be03e568c14b6b6d9084851caa7
Saul Paredes <saulkurosaki13@gmail.com>
2024-02-18 23:26:19 -0600

    feat(Profile page): implement complete Profile page component

--- app/(root)/profile/[id]/page.tsx
+++ app/(root)/profile/[id]/page.tsx
@@ -1,7 +1,15 @@
-import React from "react";
+import { getUserProjects } from "@/lib/utils";
+import { getUserById } from "@/lib/actions/user.actions";
+import ProfilePage from "@/components/ProfilePage";
 
-const Profile = () => {
-  return <div>Profile page</div>;
+const Profile = async ({ params: { id } }: { params: { id: string } }) => {
+  const user = await getUserById(id);
+  const userProjectsIds = user?.projects;
+  const userProjects: any[] = await getUserProjects(userProjectsIds);
+
+  if (!user) return <p className="no-result-text">Failed to fetch user info</p>;
+
+  return <ProfilePage user={result?.user} />;
 };
 
 export default Profile;
